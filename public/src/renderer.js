e300f3e5c82f5395c951e711f2d442095284c70b
yunseung hyun <yunseung_hyun@tmax.co.kr>
2022-04-22 10:42:19 +0900

    webpack apply

---
+++ public/src/renderer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  App,
+  StoreProvider,
+  RootStore
+} from 'outdoor-planner';
+
+const rootStore = new RootStore();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreProvider value={rootStore}>
+      <App />
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/export.js
@@ -0,0 +1,9 @@
+import App from './App';
+import { StoreProvider } from "./stores/Context";
+import { RootStore } from "./stores/RootStore";
+
+export {
+  App,
+  StoreProvider,
+  RootStore
+}
