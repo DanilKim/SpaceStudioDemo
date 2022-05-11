5f4519cd11d60d8a6dfa40fca10da418e6c074d1
DanilKim <daniel_kim@tmax.co.kr>
2022-05-11 13:53:50 +0900

    Decorating studio mode

--- src/components/Objects/Decorator.js
+++ src/components/Objects/Decorator.js
@@ -2,8 +2,8 @@ import React, { useEffect, Suspense } from 'react';
 import * as THREE from 'three';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 import { useThree } from '@react-three/fiber';
-import { Plane, Environment, useProgress, Html } from '@react-three/drei';
-import { useStores  } from '../../stores/Context';
+import { Plane, Environment, useProgress, Html, Sky, Stars, Cloud } from '@react-three/drei';
+import { useStores } from '../../stores/Context';
 import { observer } from 'mobx-react';
 
 const BASE_URL_HDRI = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port;
@@ -39,8 +39,8 @@ function Decorator() {
                         texture.dispose();
                         pmremGenerator.dispose();
                     })
-                    //---지난 편에서 업로드한 HDR 파일을 배경으로 로딩하는 과정은 설명이 복잡해서 그냥 따라하는 것이 좋습니다.
-                    //---다만, three.js 버전별로 코드가 안 먹는 경우가 있으니 버전에 유의하도록 합니다.
+                //---지난 편에서 업로드한 HDR 파일을 배경으로 로딩하는 과정은 설명이 복잡해서 그냥 따라하는 것이 좋습니다.
+                //---다만, three.js 버전별로 코드가 안 먹는 경우가 있으니 버전에 유의하도록 합니다.
             } else {
                 scene.background = new THREE.Color().setHSL(0.6, 0, 1);
                 scene.fog = new THREE.Fog(scene.background, 500, 10000);
@@ -50,7 +50,7 @@ function Decorator() {
 
         //setLight(props);
         //setGround(props);
-        if ( PlaymodeStore.playMode ) {
+        if (PlaymodeStore.playMode) {
             //setBackground();
         }
 
@@ -67,6 +67,13 @@ function Decorator() {
         >
             <meshStandardMaterial color={PlaymodeStore.playMode ? "green" : "white"} />
         </Plane>
+        {!PlaymodeStore.playMode &&
+            <Suspense fallback={null}>
+                <Sky distance={45000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
+                <Stars radius={5} depth={100} count={100} factor={4} saturation={0} fade speed={0.1} />
+                <Cloud opacity={0.5} speed={0.4} width={10} depth={-20} segments={20} />
+            </Suspense>
+        }
         <directionalLight
             castShadow
             shadow-mapSize-height={10240}
@@ -81,9 +88,9 @@ function Decorator() {
             shadow-blurSamples={5}
             position={[15, 22, 10]}
             intensity={1} />
-        { PlaymodeStore.playMode &&
-            <Suspense fallback={<Loader/>}>
-                <Environment files={HDRI} path={BASE_URL_HDRI} background/>
+        {PlaymodeStore.playMode &&
+            <Suspense fallback={<Loader />}>
+                <Environment files={HDRI} path={BASE_URL_HDRI} background />
             </Suspense>
         }
     </>);
